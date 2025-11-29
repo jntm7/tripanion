6b6e102ac9fa274f5c293c2bbeda7cf5167f2fa0
Jonathan <108718802+jntm7@users.noreply.github.com>
2025-11-28 18:04:09 -0700

    integrate firebase for Android and iOS

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,13 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
+}
+
+dependencies {
+    // Implement the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
 }
 
 android {
