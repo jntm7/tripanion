1b136ee8f81910b146a937314bc3686dd3052ce2
Jonathan <108718802+jntm7@users.noreply.github.com>
2025-12-01 18:11:13 -0700

    Create main screen with navbar

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
+import 'flights_screen.dart';
+import 'hotels_screen.dart';
+import 'favourites_screen.dart';
+import 'account_screen.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 2;
+
+  final List<Widget> _screens = [
+    const FlightsScreen(),
+    const HotelsScreen(),
+    const HomeScreen(),
+    const FavouritesScreen(),
+    const AccountScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.flight),
+            label: 'Flights',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.hotel),
+            label: 'Hotels',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Saved',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Account',
+          ),
+        ],
+      ),
+    );
+  }
+}
