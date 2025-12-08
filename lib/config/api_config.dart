1ba3090934d4ac20a033c51ded4f4c1c79948070
Jonathan <108718802+jntm7@users.noreply.github.com>
2025-12-07 17:51:38 -0700

    Uncomment .gitignore for API config file

---
+++ lib/config/api_config.dart
@@ -0,0 +1,14 @@
+class ApiConfig {
+  // Amadeus API credentials
+  // https://developers.amadeus.com/
+  static const String amadeusApiKey = 'q8gDlqM3G7GZe5Wz4SGD7xHKJ4BagH1A';
+  static const String amadeusApiSecret = 'IJxAmjXF1IBYBgYv';
+
+  // LiteAPI credentials
+  // https://liteapi.travel/
+  static const String liteApiKey = 'prod_0ec42cbe-823b-44b6-a162-0ce5d48490be'; // Production Private API Key
+
+  // Fixer.io credentials
+  // https://fixer.io/
+  static const String fixerApiKey = '9237fd3488b64088341045d3b0783a90';
+}
